9dc099d93f0bdd32118053391163cf3fd7152c16
d-stragapede <d.stragapede@elvenstudio.it>
2016-01-29 16:56:46 +0100

    [IMP] Add new module elvenstudio_product_cost_sale

---
+++ elvenstudio_product_cost_sale/__openerp__.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: ElvenStudio
+#    Copyright 2015 elvenstudio.it
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+ 'name': 'Product Cost Sale',
+ 'license': 'AGPL-3',
+ 'version': '0.1.0',
+ 'category': 'Sales',
+ 'website': 'https://github.com/ElvenStudio/Odoo',
+ 'summary': "Adds a cost_sale in product",
+ 'description': """
+Add a product cost that depends on the current stock and the supplier price.
+==============================================================
+
+This module adds a new product cost called cost_sale, that is evaluated as follow:
+ * if the product is in stock, the cost is equal to the variant cost price;
+ * if the product is not in stock, the price is got from the main supplier.
+ * if the context need a quantity Q of the product and the shock has q1 < Q the cost is:
+   cost_sale = (q1 * variant_cost_price + (Q - q1) * supplier price ) / Q.
+
+ This new cost_sale can be used in product pricelist as base cost for pricelists.
+    """,
+ 'author': "ElvenStudio",
+ 'license': 'AGPL-3',
+ 'website': 'http://www.elvenstudio.it',
+
+ 'images': ['images/elvenstudio.png', ],
+
+ 'depends': [
+     'product',
+     'product_supplierinfo_for_customer',
+     'product_variant_cost',
+ ],
+
+ 'data': [
+     'data/default.xml',
+     'views/product.xml',
+ ],
+
+ 'installable': True,
+ 'application': False,
+ }

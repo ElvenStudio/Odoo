b1f6f9c6deff85dd66072895a243649619a032e1
d-stragapede <d.stragapede@elvenstudio.it>
2016-02-19 12:15:38 +0100

    [FIX] action_cancel reset picking for invoice in draft state.

--- elvenstudio_invoice_picking_2binvoiced_fix/models/account_invoice.py
+++ elvenstudio_invoice_picking_2binvoiced_fix/models/account_invoice.py
@@ -2,6 +2,7 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
+
 from openerp import models, api, exceptions, _
 
 
@@ -29,20 +30,31 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def action_cancel(self):
+
+        # Hold on invoices state before cancel action
+        invoices = {key: state for key, state in map(lambda s: (s.id, s.state), self)}
         res = super(AccountInvoice, self).action_cancel()
+
+        # If the super action_cancel does not raise any error
+        # can be checked the relative pickings
         for invoice in self:
+            # Cant use invoice.state because is already in 'cancel' state
+            # if invoice.state not in ['draft', 'proforma', 'proforma2']:
+            if invoices[invoice.id] not in ['draft', 'proforma', 'proforma2']:
+                picking_names = []
+                if invoice.origin:
+                    if invoice.origin.find(",") >= 0:
+                        picking_names = invoice.origin.split(',')
+                    else:
+                        picking_names = [invoice.origin]
 
-            picking_names = []
-            if invoice.origin:
-                if invoice.origin.find(",") >= 0:
-                    picking_names = invoice.origin.split(',')
-                else:
-                    picking_names = [invoice.origin]
+                for picking_name in picking_names:
+                    # Remove any whitespace before search
+                    cond = [('name', '=', picking_name.replace(' ', ''))]
+                    picking = self.env['stock.picking'].search(cond, limit=1)
 
-            for picking_name in picking_names:
-                cond = [('name', '=', picking_name)]
-                picking = self.env['stock.picking'].search(cond, limit=1)
-                if picking:
-                    picking.write({'invoice_state': '2binvoiced'})
+                    # Reset picking state in 2binvoiced only if is currently set to invoiced
+                    if picking and picking.invoice_state == 'invoiced':
+                        picking.write({'invoice_state': '2binvoiced'})
 
         return res

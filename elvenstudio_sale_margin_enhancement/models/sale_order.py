482e8e6ed1b9b5bb55a75976492fb75b7be90f5c
d-stragapede <d.stragapede@elvenstudio.it>
2016-02-26 18:00:29 +0100

    [FIX] correct order product_margin computation.

--- elvenstudio_sale_margin_enhancement/models/sale_order.py
+++ elvenstudio_sale_margin_enhancement/models/sale_order.py
@@ -2,7 +2,8 @@
 
 import logging
 
-from openerp import api, models
+from openerp import api, fields, models
+import openerp.addons.decimal_precision as dp
 
 _log = logging.getLogger(__name__)
 
@@ -13,7 +14,6 @@ class SaleOrderLine(models.Model):
     @api.multi
     @api.onchange('product_id', 'product_uom_qty', 'order_id.pricelist_id')
     def product_id_change_margin(self):
-        _log.warning("product_id_change_margin")
         for line in self:
             if line.order_id.pricelist_id:
                 pricelist = line.order_id.pricelist_id
@@ -37,7 +37,6 @@ class SaleOrderLine(models.Model):
     @api.multi
     @api.depends('product_id', 'purchase_price', 'product_uom_qty', 'price_unit')
     def _product_margin(self):
-        _log.warning("_product_margin")
         for line in self:
             currency = line.order_id.pricelist_id.currency_id
             line.margin = currency.round(
@@ -48,9 +47,24 @@ class SaleOrderLine(models.Model):
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    margin = fields.Float(
+        compute='_product_margin',
+        currency_field='currency_id',
+        digits=dp.get_precision('Product Price'),
+        store=True,
+        help="It gives profitability by calculating the difference between the Unit Price and the cost."
+    )
+
+    @api.multi
+    @api.depends('order_line.margin')
+    def _product_margin(self):
+        for order in self:
+            order.margin = sum(order.order_line.filtered(lambda r: r.state != 'cancel').mapped('margin'))
+
     @api.multi
     def recompute_order_margin(self):
-        _log.warning("recompute_order_margin")
         for order in self:
             order.order_line.product_id_change_margin()
             order.order_line._product_margin()
+
+        self._product_margin()

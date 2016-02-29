52128c68feb5bfd8c6ff789c55c06b33f3c41e9f
d-stragapede <d.stragapede@elvenstudio.it>
2016-02-29 19:30:21 +0100

    [FIX] added sale order line margin field for odoo 8 compatibility.

--- elvenstudio_sale_margin_enhancement/__openerp__.py
+++ elvenstudio_sale_margin_enhancement/__openerp__.py
@@ -29,7 +29,7 @@
 Sale Margin Enhancement
 ==============================================================
 
-This module extends odoo sale_margin addind this functionalities:
+This module extends odoo sale_margin adding this functionalities:
  - get sale order line purchase price from pricelist base cost price;
  - compute margin using product_uom_qty instead of product_uos_qty;
  - add a bulk action to recalculate all sale order margin selected in tree view.

05f48abc0368cb1746a11742697dab18fc772b7b
d-stragapede <d.stragapede@elvenstudio.it>
2016-06-13 12:33:44 +0200

    [FIX] Added sudo on browse to avoid restrictions rules in odoo v7.

--- account_check_partner_credit/models/partner.py
+++ account_check_partner_credit/models/partner.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from openerp.osv import osv, fields
-# import logging
-# _log = logging.getLogger(__name__)
 
 
 class Partner(osv.osv):
@@ -17,9 +15,6 @@ class Partner(osv.osv):
     def _credit_debit_get(self, cr, uid, ids, field_names, arg, context=None):
         res = super(Partner, self)._credit_debit_get(cr, uid, ids, field_names, arg, context=context)
 
-        # _log.warning("before compute")
-        # _log.warning(res)
-
         for partner_id in res:
             checks_on_hold_id = self.pool.get('account.check').search(
                 cr, uid,
@@ -29,14 +24,13 @@ class Partner(osv.osv):
                 context=context
             )
 
-            checks_on_hold = self.pool.get('account.check').sudo().browse(cr, uid, checks_on_hold_id, context=context)
+            # Access to record with SUPERUSER ID to avoid restriction rules
+            checks_on_hold = self.pool.get('account.check').browse(cr, 1, checks_on_hold_id, context=context)
 
             checks_credit = sum(check.amount for check in checks_on_hold)
 
             res[partner_id]['credit'] += checks_credit
 
-        # _log.warning("after compute")
-        # _log.warning(res)
         return res
 
     _columns = {

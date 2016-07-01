1f3ff03ee86b37e8196fe7eeadc84484e4d43f0b
jba <v.terzulli@elvenstudio.it>
2016-07-01 18:36:13 +0200

    first commit of the credit_limit_enhancements

---
+++ elvenstudio_credit_limit_enhancements/README.md
@@ -0,0 +1,10 @@
+Credit enhancement
+==========================================================
+
+This module extends the credit limit functionalities:
+ - adds security groups to allow credit modify
+ - adds ir.mail support: a log internal message is saved on partner view when credit_limit being modified
+
+Usage:
+------
+Set in _Configuration->Users->Finance&Accounting_ the option _Can modify credit limit_ to allow users to modify credit limit.
---
+++ elvenstudio_credit_limit_enhancements/models/res_partner.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
+
+# import logging
+# _log = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def write(self, vals):
+
+        if 'credit_limit' in vals:
+            partners = self.filtered(lambda p: not p.parent_id)
+
+            for partner in partners:
+                old = str(partner.credit_limit)
+                new = str(vals['credit_limit'])
+                partner.message_post(body=_("Credit limit modified: %s &rArr; %s" % (old, new)))
+
+        return super(ResPartner, self).write(vals)

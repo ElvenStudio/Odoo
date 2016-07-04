d9ce7502704fb677b21e59e39a4738f2a77549a3
d-stragapede <d.stragapede@elvenstudio.it>
2016-07-04 18:40:26 +0200

    [ADD] POS interface customization

---
+++ elvenstudio_pos_md/static/src/js/main.js
@@ -0,0 +1,7 @@
+
+openerp.elvenstudio_pos_md = function (instance) {
+    var module = instance.point_of_sale;
+
+    elvenstudio_pos_md_screens(instance, module);
+    elvenstudio_pos_md_widgets(instance, module);
+};
---
+++ elvenstudio_pos_md/static/src/js/screens.js
@@ -0,0 +1,6 @@
+function elvenstudio_pos_md_screens(instance, module) {
+
+    var QWeb = instance.web.qweb,
+    _t = instance.web._t;
+    console.log("aweee");
+}
